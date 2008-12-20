38b05f08ac0b73a7802b6603473df5e589fe706f
Александър Димитров <aleks_d@gmx.de>
2008-12-20 22:06:08 +0100

    Added some more documentation to data structures.

--- Measures/Corpus.hs
+++ Measures/Corpus.hs
@@ -14,12 +14,14 @@ module Measures.Corpus where
 import qualified Data.Map as M
 import qualified Data.ByteString.Lazy.Char8 as L
 
+-- | A map that maps the category names to the number of times it has
+--   been used by a specific annotator.
 type Category = M.Map L.ByteString (Int,Int)
 
 -- | A strict data structure storing a corpus.
-data Corpus = Corpus { items      :: !Int
-                     , agreement  :: !Int
-                     , categories :: !Category 
+data Corpus = Corpus { items      :: !Int -- ^ The total number of items in the Corpus
+                     , agreement  :: !Int -- ^ The total number of items the two annotators agree upon
+                     , categories :: !Category  -- ^ The observed categories and their annotation counts
                      }
 
 -- | List statistics of a corpus.
