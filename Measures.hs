cc63a2a9a12cd85b8f26606f9509196e9f82746c
Александър Димитров <aleks_d@gmx.de>
2008-12-21 03:20:02 +0100

    Refactored square function.

--- Measures.hs
+++ Measures.hs
@@ -20,11 +20,11 @@ s (Corpus _ _ c) = 1.0/(fromIntegral (M.size c))
 
 -- | Pi-Measure
 pi :: Corpus -> Double 
-pi (Corpus i _ c) = (1.0/(4.0*(fromIntegral (i*i)))) * (fromIntegral (M.fold (\(a,b) n -> ((a+b)^2)+n) 0 c))
+pi (Corpus i _ c) = (1.0/(4.0*(fromIntegral (i)^2))) * (fromIntegral (M.fold (\(a,b) n -> ((a+b)^2)+n) 0 c))
 
 -- | Kappa-Measure
 kappa :: Corpus -> Double 
-kappa (Corpus i _ c) = (1.0/(fromIntegral (i*i))) * (fromIntegral (M.fold (\(a,b) n -> (a*b)^2 +n) 0 c))
+kappa (Corpus i _ c) = (1.0/(fromIntegral (i)^2)) * (fromIntegral (M.fold (\(a,b) n -> (a*b)^2 +n) 0 c))
 
 -- | Evaluate a corpus according to a corpus evaluation function (pi, kappa, s…)
 eval :: Corpus -> (Corpus -> Double) -> Double 
